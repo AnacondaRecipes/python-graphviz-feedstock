26d8b921980fa96511743b042a9833c91a1530ca
Steven <scrass@anaconda.com>
2025-03-26 09:00:43 -0600

    re-remove imports from meta.yaml, add import checks to test files

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -2,5 +2,8 @@
 set "PATH=%LIBRARY_BIN%;%PATH%"
 set "LIBARCHIVE=%LIBRARY_BIN%\libarchive.dll"
 
+:: Sanity check: does graphviz import?
+%PYTHON% -c "import graphviz" || exit /b 1
+
 pip check
 pytest tests -v
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+# Sanity check: make sure 'graphviz' can be imported before proceeding
+python -c "import graphviz"
+
 pip check
 pytest tests -v
