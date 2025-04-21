d9d5cefe0ac85df1bc4fcec391fd75db1697d730
Steven <scrass@anaconda.com>
2025-04-21 14:00:09 -0600

    stop skipping windows tests

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -6,10 +6,6 @@ import dask.array as da
 import platform
 import sys
 
-if platform.system() == "Windows":
-    print("Skipping tests on Windows")
-    sys.exit(0)
-
 x = da.ones(4, chunks=(2,))
 for fmt in ['pdf', 'png', 'dot', 'svg']:
     (x + 1).sum().visualize(filename='graph.%s' % fmt)
